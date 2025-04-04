ebac5dd9d2de2838a1cbf58b80e73d9c3bbef363
Arshjeet2003 <arshjeetsingh123@gmail.com>
2025-04-04 17:41:55 +0530

    color changes with mouse pointer

--- src/lib.rs
+++ src/lib.rs
@@ -13,31 +13,37 @@ struct State {
     queue: wgpu::Queue,
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
+    clear_color: wgpu::Color,
 }
 
 impl State {
     // Creating some of the wgpu types requires async
     async fn new(window: &Window) -> Self {
+        let clear_color = wgpu::Color {
+            r: 0.1,
+            g: 0.2,
+            b: 0.3,
+            a: 1.0,
+        };
+
         let size = window.inner_size();
 
         // The instance is a handle to our GPU
         // Backends::all => Vulkan + Metal + DX12 +
         // Browser WebGPU
-        let instance =
-            wgpu::Instance::new(wgpu::Backends::all());
-        let surface =
-            unsafe { instance.create_surface(window) };
+        let instance = wgpu::Instance::new(wgpu::Backends::all());
+        let surface = unsafe { instance.create_surface(window) };
         let adapter = instance
             .request_adapter(&wgpu::RequestAdapterOptions {
-                power_preference:
-                    wgpu::PowerPreference::default(),
+                power_preference: wgpu::PowerPreference::default(),
                 compatible_surface: Some(&surface),
                 force_fallback_adapter: false,
             })
             .await
             .unwrap();
 
-        let (device, queue) = adapter.request_device(
+        let (device, queue) = adapter
+            .request_device(
                 &wgpu::DeviceDescriptor {
                     features: wgpu::Features::empty(),
                     // WebGL doesn't support all of wgpu's features, so if
@@ -50,12 +56,13 @@ impl State {
                     label: None,
                 },
                 None, // Trace path
-            ).await.unwrap();
+            )
+            .await
+            .unwrap();
 
         let config = wgpu::SurfaceConfiguration {
             usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
-            format: surface.get_supported_formats(&adapter)
-                [0],
+            format: surface.get_supported_formats(&adapter)[0],
             width: size.width,
             height: size.height,
             present_mode: wgpu::PresentMode::Fifo,
@@ -67,23 +74,37 @@ impl State {
             queue,
             config,
             size,
+            clear_color,
         }
     }
 
-    fn resize(
-        &mut self,
-        new_size: winit::dpi::PhysicalSize<u32>,
-    ) {
+    fn resize(&mut self, new_size: winit::dpi::PhysicalSize<u32>) {
         if new_size.width > 0 && new_size.height > 0 {
             self.size = new_size;
             self.config.width = new_size.width;
             self.config.height = new_size.height;
-            self.surface
-                .configure(&self.device, &self.config);
+            self.surface.configure(&self.device, &self.config);
         }
     }
 
     fn input(&mut self, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::CursorMoved {
+                device_id,
+                position,
+                modifiers,
+            } => {
+                let r = position.x / self.size.width as f64;
+                let g = position.y / self.size.height as f64;
+                self.clear_color = wgpu::Color {
+                    r: r,
+                    g: g,
+                    b: 0.3,
+                    a: 1.0,
+                }
+            }
+            _ => {}
+        }
         false
     }
 
@@ -93,44 +114,31 @@ impl State {
 
     fn render(&mut self) -> Result<(), wgpu::SurfaceError> {
         let output = self.surface.get_current_texture()?;
-        let view = output.texture.create_view(
-            &wgpu::TextureViewDescriptor::default(),
-        );
-        let mut encoder =
-            self.device.create_command_encoder(
-                &wgpu::CommandEncoderDescriptor {
-                    label: Some("Render Encoder"),
-                },
-            );
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = self
+            .device
+            .create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Encoder"),
+            });
         {
-            let _render_pass = encoder.begin_render_pass(
-                &wgpu::RenderPassDescriptor {
-                    label: Some("Render Pass"),
-                    color_attachments: &[Some(
-                        wgpu::RenderPassColorAttachment {
-                            view: &view,
-                            resolve_target: None,
-                            ops: wgpu::Operations {
-                                load: wgpu::LoadOp::Clear(
-                                    wgpu::Color {
-                                        r: 0.1,
-                                        g: 0.2,
-                                        b: 0.3,
-                                        a: 1.0,
-                                    },
-                                ),
-                                store: true,
-                            },
-                        },
-                    )],
-                    depth_stencil_attachment: None,
-                },
-            );
+            let _render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(self.clear_color),
+                        store: true,
+                    },
+                })],
+                depth_stencil_attachment: None,
+            });
         }
 
         // submit will accept anything that implements IntoIter
-        self.queue
-            .submit(std::iter::once(encoder.finish()));
+        self.queue.submit(std::iter::once(encoder.finish()));
         output.present();
 
         Ok(())
@@ -147,12 +155,11 @@ pub async fn run() {
             tracing_subscriber::fmt::init()
         }
     }
-    info!("info!!!");
-    warn!("warning");
-    error!("eeeeeek");
+    // info!("info!!!");
+    // warn!("warning");
+    // error!("eeeeeek");
     let event_loop = EventLoop::new();
-    let window =
-        WindowBuilder::new().build(&event_loop).unwrap();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
 
     #[cfg(target_arch = "wasm32")]
     {
@@ -165,10 +172,8 @@ pub async fn run() {
         web_sys::window()
             .and_then(|win| win.document())
             .and_then(|doc| {
-                let dst =
-                    doc.get_element_by_id("wasm-example")?;
-                let canvas =
-                    web_sys::Element::from(window.canvas());
+                let dst = doc.get_element_by_id("wasm-example")?;
+                let canvas = web_sys::Element::from(window.canvas());
                 dst.append_child(&canvas).ok()?;
                 Some(())
             })
@@ -181,27 +186,30 @@ pub async fn run() {
         Event::WindowEvent {
             ref event,
             window_id,
-        } if window_id == window.id() => if !state.input(event) {
-             match event {
-            WindowEvent::CloseRequested
-            | WindowEvent::KeyboardInput {
-                input:
-                    KeyboardInput {
-                        state: ElementState::Pressed,
-                        virtual_keycode: Some(VirtualKeyCode::Escape),
+        } if window_id == window.id() => {
+            if !state.input(event) {
+                match event {
+                    WindowEvent::CloseRequested
+                    | WindowEvent::KeyboardInput {
+                        input:
+                            KeyboardInput {
+                                state: ElementState::Pressed,
+                                virtual_keycode: Some(VirtualKeyCode::Escape),
+                                ..
+                            },
                         ..
-                    },
-                ..
-            } => *control_flow = ControlFlow::Exit,
-            WindowEvent::Resized(physical_size) => {
-                state.resize(*physical_size);
+                    } => *control_flow = ControlFlow::Exit,
+                    WindowEvent::Resized(physical_size) => {
+                        state.resize(*physical_size);
+                    }
+                    WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
+                        // new_inner_size is &&mut so we have to dereference it twice
+                        state.resize(**new_inner_size);
+                    }
+                    _ => {}
+                }
             }
-            WindowEvent::ScaleFactorChanged { new_inner_size, .. } => {
-                // new_inner_size is &&mut so we have to dereference it twice
-                state.resize(**new_inner_size);
-            }
-            _ => {}
-        }},
+        }
         Event::RedrawRequested(window_id) if window_id == window.id() => {
             state.update();
             match state.render() {
